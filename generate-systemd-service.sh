f016d720bb77e67257e188a02b67381c5f3162ed
Lu Stadler <lu@papill0n.org>
2019-07-28 19:00:33 +0200

    Improve generated systemd file
    Also remove the existing full service file in favour of the generated
    one.

--- generate-systemd-service.sh
+++ generate-systemd-service.sh
@@ -3,6 +3,7 @@
 BASE_URL="${BASE_URL:-http://localhost:9999}"
 BINARY_PATH="${BINARY_PATH:-/usr/bin/share}"
 RUN_DIR="${RUN_DIR:-/srv/share}"
+UPLOADS_DIR="${UPLOADS_DIR:-$RUN_DIR/uploads}"
 
 cat <<-EOF
 [Unit]
@@ -10,6 +11,9 @@ Description=share - simple filesharing service
 
 [Service]
 ExecStart=BASE_URL=$BASE_URL $BINARY_PATH
+ProtectSystem=strict
+ReadWritePaths=$UPLOADS_DIR
+ReadOnlyPaths=$RUN_DIR/upload-secret.txt $RUN_DIR/admin-secret.txt $RUN_DIR/validation.js
 
 [Install]
 WantedBy=multi-user.target
